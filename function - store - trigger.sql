8f1dd321d4081d4e65bbf2ae70bb058f7385ac55
quyetthang96 <quyetthang326@gmail.com>
2017-05-25 10:41:42 +0700

    store

--- function - store - trigger.sql
+++ function - store - trigger.sql
@@ -11,3 +11,44 @@ begin
 		end
 	return @ma
 end
+-----------------nhanvien--------------
+create proc ThemNhanvien
+@ma	varchar(10),
+@mahienthi	nvarchar(20),
+@hoten	nvarchar(200),
+@quequan	nvarchar(100),
+@diachi	nvarchar(100),
+@gioitinh	int,
+@ngaysinh	date,
+@luong	int,
+@chucvu	nvarchar(100),
+@id varchar(20),
+@pass varchar(100)
+as
+begin
+	insert into nhanvien values(@ma,@mahienthi,@hoten,@quequan,@diachi,@gioitinh,@ngaysinh,@luong,@chucvu,@id,@pass)
+end
+----
+create proc UpdateNhanvien
+@ma	varchar(10),
+@mahienthi	nvarchar(20),
+@hoten	nvarchar(200),
+@quequan	nvarchar(100),
+@diachi	nvarchar(100),
+@gioitinh	int,
+@ngaysinh	date,
+@luong	int,
+@chucvu	nvarchar(100),
+@id varchar(20),
+@pass varchar(100)
+as
+begin
+	Update nhanvien set mahienthi = @mahienthi,hoten = @hoten, quequan = @quequan,diachi= @diachi,gioitinh = @gioitinh,ngaysinh = @ngaysinh,luong = @luong,chucvu = @chucvu,@id=id,@pass=pass where ma = @ma
+end
+----
+create proc XoaNhanvien
+@ma	varchar(10)
+as
+begin
+	delete from nhanvien where ma = @ma
+end 
