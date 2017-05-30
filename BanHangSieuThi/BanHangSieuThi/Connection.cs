d3d771e7f3ed63d8e30026cdf5c74d054f927288
hoaipanda <thuhoai2901@gmail.com>
2017-05-30 21:59:05 +0700

    thêm hàng
    thêm mới và sửa

--- BanHangSieuThi/BanHangSieuThi/Connection.cs
+++ BanHangSieuThi/BanHangSieuThi/Connection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,36 @@ namespace BanHangSieuThi
 {
     class Connection
     {
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        public static SqlDataAdapter da;
         public static SqlConnection Conn()
         {
            string connString = @"Data Source=SUPER\SQLEXPRESS ;Initial Catalog=TTN_banhangsieuthi ;Persist Security Info=True; User ID=detai6 ;Password=detai6 ";
            SqlConnection conn = new SqlConnection(connString);
            return conn;
+        }
+        public static DataTable getDataTable(string sql)
+        {
+            // tạo đối tượng trỏ tới dl trong database 
+            cmd = new SqlCommand(sql, conn);
+            // dtg để lưu dữ liệu lên database 
+            da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            // tạo database để hiển thị dl
+            DataTable table = new DataTable();
+            da.Fill(table);
+            da.Dispose();
+            cmd.Dispose();
+            return table;
+        }
+        public static void Excute(string sql)
+        {
+            Conn();
+            cmd = new SqlCommand(sql, conn);
+            //gọi hàm để có thể thực hiện thao tác Delete Insert Update
+            cmd.ExecuteNonQuery();
+
         }
        
     }
--- BanHangSieuThi/BanHangSieuThi/Themhang.cs
+++ BanHangSieuThi/BanHangSieuThi/Themhang.cs
@@ -12,9 +12,160 @@ namespace BanHangSieuThi
 {
     public partial class Themhang : Form
     {
+        public void showData()
+        {
+            Connection.Conn();
+            var vv = Connection.getDataTable("Select * from hanghoa");
+            vv.Columns["ma"].ColumnName = "Mã Hàng Hóa";
+            vv.Columns["mahienthi"].ColumnName = "Mã Hàng Hóa";
+            vv.Columns["ten"].ColumnName = "Tên Hàng Hóa";
+            vv.Columns["soluongcon"].ColumnName = "Số Lượng";
+            vv.Columns["loaihang"].ColumnName = "Loại Hàng";
+            vv.Columns["gia"].ColumnName = "Giá";
+            dtHanghoa.DataSource = vv;
+
+        }
+        public void Clear()
+        {
+            txtMaHH.Clear();
+            txtMaHT.Clear();
+            txtTenHH.Clear();
+            txtSL.Clear();
+            txtLoaiHang.Clear();
+            txtGia.Clear();
+            txtMaHH.Focus();
+        }
         public Themhang()
         {
             InitializeComponent();
         }
+
+        private void bntclear_Click(object sender, EventArgs e)
+        {
+            this.Clear();
+        }
+
+        private void bntThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string a = txtMaHH.Text;
+                string b = txtMaHT.Text;
+                string c = txtTenHH.Text;
+                string d = txtSL.Text;
+                string g = txtLoaiHang.Text;
+                string h = txtGia.Text;
+                string sql = "Insert into ThietBi values( N'" + a + "' , N'" + b + "' ,N'" + c + "',N'" + d + "',N'" + g + "',N'" + h + "')";
+                Connection.Excute(sql);
+                //this.showData("Select * from hanghoa");
+                this.showData("Select * from hanghoa");
+                this.Clear();
+                MessageBox.Show("Thêm mới thành công!");
+            }
+
+            catch
+            {
+                MessageBox.Show("Không thêm mới được!");
+            }
+        }
+
+        private void showData(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dtHanghoa_SelectionChanged(object sender, EventArgs e)
+        {
+            int chiso = dtHanghoa.CurrentRow.Index;
+            txtMaHH.Text = dtHanghoa[0, chiso].Value.ToString();
+            txtMaHT.Text = dtHanghoa[1, chiso].Value.ToString();
+            txtTenHH.Text = dtHanghoa[2, chiso].Value.ToString();
+            txtSL.Text = dtHanghoa[3, chiso].Value.ToString();
+            txtLoaiHang.Text = dtHanghoa[4, chiso].Value.ToString();
+            txtGia.Text = dtHanghoa[5, chiso].Value.ToString();
+            txtMaHH.Enabled = false;
+        }
+
+        private void txtMaHT_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtTenHH.Focus();
+        }
+
+        private void txtTenHH_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtSL.Focus();
+        }
+
+        private void txtSL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtLoaiHang.Focus();
+        }
+
+        private void txtLoaiHang_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtGia.Focus();
+        }
+
+        private void bntSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int chiso = dtHanghoa.CurrentRow.Index;
+                string a = txtMaHT.Text;
+                string b = txtTenHH.Text;
+                string c = txtSL.Text;
+                string d = txtLoaiHang.Text;
+                string g = txtGia.Text;
+                string sql = "Update Hanghoa set mahienthi = N'" + a + "' ,  ten = N'" + b + "' , soluong = N'" + c + "' ,loaihangcon = N'" + d + "',gia = N'" + g + "'  where ma = '" + dtHanghoa[0, chiso].Value.ToString() + "' ";
+                Connection.Excute(sql);
+                this.showData("Select * from hanghoa");
+                this.Clear();
+                MessageBox.Show("Sửa thành công!");
+            }
+
+            catch
+            {
+                MessageBox.Show("Không sửa được!");
+            }
+        }
+
+        private void Themhang_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int chiso = dtHanghoa.CurrentRow.Index;
+                string sql = "Delete from ThietBi where ma = '" + dtHanghoa[0, chiso].Value.ToString() + "' ";
+                Connection.Excute(sql);
+                this.showData("Select * from hanghoa");
+                this.Clear();
+                MessageBox.Show("Xóa thành công!");
+            }
+
+            catch
+            {
+                MessageBox.Show("Không xóa được!");
+            }
+        }
+
+        private void bntThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        } 
+
+
+
+
+
+       
     }
 }
